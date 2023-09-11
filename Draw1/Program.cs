905a023926c5f5036d48f89d02d5764408d63a6f
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-11 15:12:41 +0200

    Add project files.

---
+++ Draw1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("        /|");
+            Console.WriteLine("       / |");
+            Console.WriteLine("      /  |");
+            Console.WriteLine("     /___|");
+            Console.ReadLine();
+        }
+    }
+}
